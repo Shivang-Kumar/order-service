65e92531393c23e7a0afe56e4e93af26c332ecd1
Shivang Kumar <2002shiva.2002@gmail.com>
2026-01-08 22:55:18 +0530

    Added FeingClient library to make synchronous api call to other services

--- src/main/java/com/practice/microservices/order_Service/controller/OrderController.java
+++ src/main/java/com/practice/microservices/order_Service/controller/OrderController.java
@@ -1,4 +1,4 @@
-package com.practice.microservices.controller;
+package com.practice.microservices.order_Service.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/com/practice/microservices/order_Service/service/OrderService.java
+++ src/main/java/com/practice/microservices/order_Service/service/OrderService.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.practice.microservices.order_Service.client.InventoryClient;
 import com.practice.microservices.order_Service.dto.OrderRequest;
 import com.practice.microservices.order_Service.model.Order;
 import com.practice.microservices.order_Service.repository.OrderRepository;
@@ -15,18 +16,33 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
 	
 	private final OrderRepository orderRepository;
-	
+	private final InventoryClient inventoryClient;
 	
 	public void placeOrder(OrderRequest orderRequest) {
-		//map order request to order object 
-		Order order=new Order();
-		order.setOrderNumber(UUID.randomUUID().toString());
-		order.setPrice(orderRequest.price());
-		order.setOrderNumber(orderRequest.orderNumber());
-		order.setQuantity(orderRequest.quantity());
-		order.setSkuCode(orderRequest.skuCode());
-		//save order to order repository
-		orderRepository.save(order);
+		
+		
+		
+		
+		//Invoking feing client to make a http request
+		var productInStock= inventoryClient.isInStock(orderRequest.skuCode(),orderRequest.quantity());
+		
+		if(productInStock)
+		{
+			//map order request to order object 
+			Order order=new Order();
+			order.setOrderNumber(UUID.randomUUID().toString());
+			order.setPrice(orderRequest.price());
+			order.setOrderNumber(orderRequest.orderNumber());
+			order.setQuantity(orderRequest.quantity());
+			order.setSkuCode(orderRequest.skuCode());
+			//save order to order repository
+			orderRepository.save(order);
+		}
+		else {
+			throw new RuntimeException("Product with SkuCode"+orderRequest.skuCode()+"is not in stock");
+		}
+		
+		
 		
 	}
 
