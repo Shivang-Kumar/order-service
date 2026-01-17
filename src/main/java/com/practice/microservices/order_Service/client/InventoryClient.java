88c42687b8c85fe62ae274942b358c405b99fbf7
Shivang Kumar <2002shiva.2002@gmail.com>
2026-01-17 23:48:42 +0530

    Update Url endpoints

--- src/main/java/com/practice/microservices/order_Service/client/InventoryClient.java
+++ src/main/java/com/practice/microservices/order_Service/client/InventoryClient.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value="inventory", url="http://localhost:8082")
+@FeignClient(value="inventory", url="http://localhost:8089")
 public interface InventoryClient {
 	
 	@RequestMapping(method=RequestMethod.GET,value="/api/inventory")
